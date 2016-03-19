7c0e676a09bd5933a8021563313456653aec90b9
gihwang <giyeop.hwang@stonybrook.edu>
2016-03-18 20:00:21 -0400

    Code is modulized.

--- 320sh.c
+++ 320sh.c
@@ -1,26 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 // Assume no input line will be longer than 1024 bytes
 #define MAX_INPUT 1024
 
-int main (int argc, char ** argv, char **envp) {
+#define STDIN     0
+#define STDOUT    1
+#define STDERR    2
 
-  int finished = 0;
+typedef enum _bool {
+  false = 0,
+  true = 1
+} bool;
+
+/* It might be changed for Async-signal-safe function. (read) */
+char * readLine() {
+  char *line = NULL;
+  size_t len = 0;
+  getline(&line, &len, stdin);
+  return line;
+}
+
+int main(int argc, char ** argv, char **envp) {
+  
+  char *prompt = "320sh> ";
+  char *line = NULL;
+
+  while(true) {
+
+    if (write(STDOUT, prompt, strlen(prompt)) == 0) { 
+      break;
+    }
+    
+    line = readLine();
+    write(STDOUT, line, strnlen(line, MAX_INPUT));
+
+  // Execute the command, handling built-in commands separately 
+  // Just echo the command line for now
+  // write(1, cmd, strnlen(cmd, MAX_INPUT));
+  }
+
+  return 0;
+}
+
+/*
+
+int main(int argc, char ** argv, char **envp) {
+
   char *prompt = "320sh> ";
   char cmd[MAX_INPUT];
+  char *line = NULL;
 
-  while (!finished) {
+  while(true) {
     char *cursor;
     char last_char;
     int rv;
     int count;
 
     // Print the prompt
-    rv = write(1, prompt, strlen(prompt));
-    if (!rv) { 
-      finished = 1;
+    rv = write(STDOUT, prompt, strlen(prompt));
+    if (rv == 0) { 
       break;
     }
     
@@ -29,20 +70,19 @@ int main (int argc, char ** argv, char **envp) {
       rv && (++count < (MAX_INPUT-1)) && (last_char != '\n');
       cursor++) { 
 
-      rv = read(0, cursor, 1);
+      rv = read(STDIN, cursor, 1);
       last_char = *cursor;
     
       if(last_char == 3) {
-        write(1, "^c", 2);
+        write(STDOUT, "^c", 2);
       } else {
-       write(1, &last_char, 1);
+       write(STDOUT, &last_char, 1);
       }
     }
 
     *cursor = '\0';
 
-    if (!rv) { 
-      finished = 1;
+    if (rv == 0) { 
       break;
     }
 
@@ -53,3 +93,5 @@ int main (int argc, char ** argv, char **envp) {
 
   return 0;
 }
+
+*/
